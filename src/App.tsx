36412a5df2088fd7a41189811e23bc4bada7d2d3
viniciusmecosta <costavinicius315@gmail.com>
2025-04-09 11:40:36 -0300

    post in api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
+
 function App() {
   const url: string = "http://127.0.0.1:8002/notify/email";
 
   const [emails, setEmails] = useState([]);
+  const [name, setName] = useState("");
+  const [emailPost, setEmailPost] = useState("");
+
+  //get
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(url);
@@ -13,6 +18,21 @@ function App() {
 
     fetchData();
   }, []);
+
+  //post
+  const handleSubmit = async (e: FormEvent<Element>): Promise<void> => {
+    e.preventDefault();
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name, email: emailPost }),
+    });
+    setName("");
+    setEmailPost("");
+  };
+
   return (
     <>
       <h1>Lista de Email's</h1>
@@ -23,6 +43,29 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-email">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              value={emailPost}
+              name="emailPost"
+              onChange={(e) => setEmailPost(e.target.value)}
+            ></input>
+          </label>
+          <input type="submit" value={"Criar"}></input>
+        </form>
+      </div>
     </>
   );
 }
